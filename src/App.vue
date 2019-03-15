d8ba1d91c515ab5fcc6e163712dec0a7d143ed94
zhouyue <18779106195@163.com>
2019-03-15 18:47:38 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.jpg">
+    <!--<router-view/>-->
+    <div id="app-in">
+      {{message}}
+    </div>
+  </div>
+</template>
+
+<script>
+import first from './components/First.vue'
+export default {
+  name: 'App',
+  data () {
+    return {
+      message: '周越是我爸爸'
+    }
+  },
+  components:{first}
+
+}
+
+
+
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/First.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="first">
+        <h1>我是标题</h1>
+        <a href="">written by {{author}}</a>
+    </div>
+</template>
+<script>
+export default {
+    data () {
+        return {
+            author: "周越啊"
+        }
+    }
+}
+</script>
